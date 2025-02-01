9b6a9577873f7e400d4dc87802564348067a1290
Berteyz <126805776+Berteyz@users.noreply.github.com>
2025-02-01 13:07:04 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,1 @@
-<?php
-$dbms = 'sqlite3'; // Тип базы данных
-$dbhost = ''; // Оставьте пустым для SQLite
-$dbport = ''; // Оставьте пустым для SQLite
-$dbname = '/var/www/html/phpbb/store/phpbb.sqlite3'; // Полный путь к файлу базы данных
-$dbuser = ''; // Оставьте пустым для SQLite
-$dbpasswd = ''; // Оставьте пустым для SQLite
-$table_prefix = 'phpbb_'; // Префикс таблиц
-$acm_type = 'file';
-$phpbb_adm_relative_path = 'adm/';
-@define('PHPBB_INSTALLED', true);
-?>
+
